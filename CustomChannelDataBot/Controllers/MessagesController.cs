408a7be470408a2243d27c32402a5bf8aa1ef240
James Mann <james.mann@bcs.org>
2017-03-12 19:48:01 +0000

    Add project files.

---
+++ CustomChannelDataBot/Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CustomChannelDataBot.Controllers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace CustomChannelDataBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new ChannelDataDialog());
+            }
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ CustomChannelDataBot/Dialogs/ChannelDataDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json.Linq;
+
+namespace CustomChannelDataBot.Controllers
+{
+    [Serializable]
+    public class ChannelDataDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedStart);
+        }
+
+        public async Task MessageReceivedStart(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var reply = context.MakeMessage();
+
+            var attachment = new
+            {
+                type = "template",
+                payload = new
+                {
+                    template_type = "airline_checkin",
+                    intro_message = "Check-in is available now.",
+                    locale = "en_US",
+                    pnr_number = "ABCDEF",
+                    checkin_url = "https://www.airline.com/check-in",
+                    flight_info = new[]
+                    {
+                        new
+                        {
+                            flight_number = "f001",
+                            departure_airport =
+                            new
+                            {
+                                airport_code = "SFO",
+                                city = "San Francisco",
+                                terminal = "T4",
+                                gate = "G8"
+                            },
+                            arrival_airport = new
+                            {
+                                airport_code = "SEA",
+                                city = "Seattle",
+                                terminal = "T4",
+                                gate = "G8"
+                            },
+                            flight_schedule = new
+                            {
+                                boarding_time = "2016-01-05T15:05",
+                                departure_time = "2016-01-05T15:45",
+                                arrival_time = "2016-01-05T17:30"
+                            }
+                        }
+                    }
+                }
+            };
+
+            reply.ChannelData = JObject.FromObject(new {attachment});
+            
+            await context.PostAsync(reply);
+            context.Wait(MessageReceivedStart);
+        }
+    }
+}
